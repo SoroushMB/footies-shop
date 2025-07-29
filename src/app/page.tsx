c0861f8820f718c7dc16ab159d411eafc2596e82
Aryo <footiesshop.customerservice@gmail.com>
2025-07-29 19:15:32 +0000

    when the screen is smaller, i cant navigate to the other items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -143,8 +143,8 @@ export default function HomePage() {
               </CarouselItem>
             ))}
           </CarouselContent>
-          <CarouselPrevious className="absolute left-0 top-1/2 -translate-y-1/2 z-10 hidden md:flex" />
-          <CarouselNext className="absolute right-0 top-1/2 -translate-y-1/2 z-10 hidden md:flex" />
+          <CarouselPrevious className="absolute left-2 top-1/2 -translate-y-1/2 z-10" />
+          <CarouselNext className="absolute right-2 top-1/2 -translate-y-1/2 z-10" />
         </Carousel>
       </section>
     </div>
