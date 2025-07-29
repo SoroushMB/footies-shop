78f4d302eca7692447655861146717cd14aad39e
Aryo <footiesshop.customerservice@gmail.com>
2025-07-29 05:47:54 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <svg xmlns="http:/

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -34,7 +34,11 @@ export const Icons = {
   ),
   apparel: (props: SVGProps<SVGSVGElement>) => (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
-      <path d="M20.38 3.46 16 2a4 4 0 0 0-8 0L3.62 3.46a2 2 0 0 0-1.34 2.23l.58 3.47a1 1 0 0 0 .99.84H6v10c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V10h2.15a1 1 0 0 0 .99-.84l.58-3.47a2 2 0 0 0-1.34-2.23z"/>
+        <path d="M21 7.33V17a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V7.33"/>
+        <path d="M15 4l-3-3-3 3"/>
+        <path d="M12 1v12"/>
+        <path d="M19.5 12.5a2.5 2.5 0 0 1-5 0V7.33"/>
+        <path d="M4.5 12.5a2.5 2.5 0 0 0 5 0V7.33"/>
     </svg>
   ),
   gloves: (props: SVGProps<SVGSVGElement>) => (
