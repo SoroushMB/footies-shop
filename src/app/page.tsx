64f7561677b20556db7463833e0efd5cc404e974
Aryo <footiesshop.customerservice@gmail.com>
2025-07-30 19:21:17 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,7 +79,7 @@ export default function HomePage() {
     { src: 'https://assets.goal.com/images/v3/blte8dcd83e6b8bc7d0/GOAL%20-%20Blank%20WEB%20-%20Facebook%20(30).jpg?auto=webp&format=pjpg&width=2048&quality=60', alt: 'Player kicking a football', hint: 'football player' },
     { src: 'https://www.elverys.ie/cdn/shop/files/Collection_Banner_ManCity_Home_25Q2_16x9_Desktop.jpg?v=1747062067&width=2400', alt: 'Football stadium', hint: 'football stadium' },
     { src: 'https://botasmaniacos.com/cdn/shop/files/F3151815-EFB3-4895-B9A7-CD5BF2E00479.jpg?v=1734387883&width=1646', alt: 'New football gear', hint: 'football cleats' },
-    { src: 'https://placehold.co/1920x1080.png', alt: 'Fans cheering', hint: 'football fans' },
+    { src: 'https://images.unsplash.com/photo-1597274747316-808c6786c165?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw3fHxzb2NjZXIlMjBib290c3xlbnwwfHx8fDE3NTM5MDMxOTV8MA&ixlib=rb-4.1.0&q=80&w=1080', alt: 'Fans cheering', hint: 'football boots' },
     { src: 'https://placehold.co/1920x1080.png', alt: 'Action shot', hint: 'football action' },
     { src: 'https://placehold.co/1920x1080.png', alt: 'Team huddle', hint: 'team huddle' },
   ];
